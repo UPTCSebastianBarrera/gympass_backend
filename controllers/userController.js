0641978eda200d749b1422fa18c7f8a7a648b542
UPTCSebastianBarrera <86314559+UPTCSebastianBarrera@users.noreply.github.com>
2024-07-22 23:22:47 -0500

    login backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,22 @@
+// controllers/userController.js
 const asyncHandler = require('express-async-handler');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Public
 const getUsers = asyncHandler(async (req, res) => {
   const users = await User.find({});
   res.json(users);
 });
 
+// @desc    Create a user
+// @route   POST /api/users
+// @access  Public
 const createUser = asyncHandler(async (req, res) => {
   const { name, email, password, address, phone, profilePicture } = req.body;
-  
-  console.log('Creating user:', req.body); // Add this line to log the incoming request
 
   const userExists = await User.findOne({ email });
 
@@ -33,12 +39,48 @@ const createUser = asyncHandler(async (req, res) => {
 
   const createdUser = await user.save();
 
-  console.log('User created:', createdUser); // Add this line to log the created user
-
-  res.status(201).json(createdUser);
+  res.status(201).json({
+    _id: createdUser._id,
+    name: createdUser.name,
+    email: createdUser.email,
+    profilePicture: createdUser.profilePicture,
+    token: generateToken(createdUser._id),
+  });
 });
+
+// @desc    Authenticate a user
+// @route   POST /api/users/login
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+  const { emailOrName, password } = req.body;
+
+  const user = await User.findOne({
+    $or: [{ email: emailOrName }, { name: emailOrName }],
+  });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      profilePicture: user.profilePicture,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email/name or password');
+  }
+});
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
 
 module.exports = {
   getUsers,
   createUser,
+  authUser,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,9 @@
+// routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
-const { getUsers, createUser } = require('../controllers/userController');
+const { getUsers, createUser, authUser } = require('../controllers/userController');
 
 router.route('/').get(getUsers).post(createUser);
+router.post('/login', authUser);
 
 module.exports = router;
